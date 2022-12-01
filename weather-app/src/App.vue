b1fec2503a5bf64d40e18bdc6c952343b0bd3ad4
Stefan Markov <stef4otm@gmail.com>
2022-12-01 13:35:59 +0200

    01.12.23

--- weather-app/src/App.vue
+++ weather-app/src/App.vue
@@ -1,8 +1,42 @@
 <template>
-  <div id="app">
+  <div class="main">
+    <Navigation class="navigation" />
     <router-view />
   </div>
 </template>
+
+<script>
+import axios from "axios";
+import Navigation from "@/components/Navigation.vue";
+
+export default {
+  name: "App",
+  components: { Navigation },
+  data() {
+    return {
+      APIKey: process.env.VUE_APP_OPENWEATHER_API_KEY,
+      city: "Sofia",
+    };
+  },
+  created() {
+    this.getCurrentWeather();
+  },
+
+  methods: {
+    getCurrentWeather() {
+      axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${
+            this.city
+          },${"BG"}&units=metric&appid=${this.APIKey}`
+        )
+        .then((responce) => {
+          console.log(responce);
+        });
+    },
+  },
+};
+</script>
 
 <style lang="scss">
 * {
@@ -10,5 +44,17 @@
   padding: 0;
   box-sizing: border-box;
   font-family: "Quicksand", sans-serif;
+}
+
+.main {
+  height: 100vh;
+  .navigation {
+    z-index: 99;
+    position: fixed;
+    max-width: 1024px;
+    width: 100%;
+    box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
+      0 2px 4px -1px rgba(0, 0, 0, 0.06);
+  }
 }
 </style>
---
+++ weather-app/src/components/Navigation.vue
@@ -0,0 +1,19 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <header class="container add-city">
+    <nav>
+      <span>Add City</span>
+      <div class="right">
+        <i class="far fa-edit"></i>
+        <i class="fas fa-sync"></i>
+        <i class="fas fa-plus"></i>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
